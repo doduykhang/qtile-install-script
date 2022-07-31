2a21c30ca2572fc24b71114983f6208dd1b1c6da
khang <dokhang295@gmail.com>
2022-07-31 15:41:22 +0700

    first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -r ~/.config/qtile
+git clone https://github.com/doduykhang/qtile-config.git ~/.config/qtile/
+sudo unzip -o ./dependencies/JetBrainsMono.zip -d /usr/share/fonts/
+sudo unzip -o ./dependencies/Takao.zip -d /usr/share/fonts/
+fc-cache -f -v
