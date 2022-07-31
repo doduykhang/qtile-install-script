19311bd18d68045706bed848500b8fb82a1cf5b6
khang <dokhang295@gmail.com>
2022-07-31 16:13:41 +0700

    update arch script add nitrogen

--- install-arch.sh
+++ install-arch.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+#install and clone qtile config
 sudo pacman -S qtile
 rm -r ~/.config/qtile
 git clone https://github.com/doduykhang/qtile-config.git ~/.config/qtile/
+
+#install font
 sudo unzip -o ./dependencies/JetBrainsMono.zip -d /usr/share/fonts/
 sudo unzip -o ./dependencies/Takao.zip -d /usr/share/fonts/
 fc-cache -f -v
+
+#set wallpaper 
+sudo pacman -S nitrogen
+rm -r ~/.config/wallpapers
+git clone https://github.com/doduykhang/wallpapers.git ~/.config/wallpapers/
+nitrogen --set-auto ~/.config/wallpapers/sakura.jpg
+
 
